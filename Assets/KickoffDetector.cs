ce9af3223941ecbc5779fecf5f6e5ee2c713c673
JohnnyChen535 <sc10167@nyu.edu>
2025-10-20 13:48:19 -0400

    layer detection

--- Assets/KickoffDetector.cs
+++ Assets/KickoffDetector.cs
@@ -2,26 +2,48 @@ using UnityEngine;
 
 public class KickoffDetector : MonoBehaviour
 {
-    private bool kickoffPossible;
+    [Header("Detection Settings")]
+    [SerializeField] private LayerMask validLayers;
+    [SerializeField] private bool debug = false;
+
+    private bool kickoffPossible = false;
+    private int overlapCount = 0;
 
     public bool GetKickoffPossible() => kickoffPossible;
 
-    void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.attachedRigidbody != null)
+        if (((1 << other.gameObject.layer) & validLayers) != 0)
+        {
+            overlapCount++;
             kickoffPossible = true;
+
+            if (debug)
+            {
+                Debug.Log($"[KickoffDetector] Entered: {other.gameObject.name} (layer: {LayerMask.LayerToName(other.gameObject.layer)})");
+            }
+        }
     }
 
-    void OnTriggerExit2D(Collider2D other)
+    private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.attachedRigidbody != null)
-            kickoffPossible = false;
+        if (((1 << other.gameObject.layer) & validLayers) != 0)
+        {
+            overlapCount--;
+            kickoffPossible = overlapCount > 0;
+
+            if (debug)
+            {
+                Debug.Log($"[KickoffDetector] Exited: {other.gameObject.name} (layer: {LayerMask.LayerToName(other.gameObject.layer)})");
+            }
+        }
     }
 
-
-    void Update()
+    private void Update()
     {
-        Debug.Log("Kickoff Possible: " + kickoffPossible);
-
+        if (debug)
+        {
+            Debug.Log($"[KickoffDetector] Kickoff Possible: {kickoffPossible} | Overlaps: {overlapCount}");
+        }
     }
 }
